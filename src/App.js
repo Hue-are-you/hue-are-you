8fe4d8997bf97d87fcd9537bbf40c223e2037978
Boris <ulanowicz_b@hotmail.com>
2020-12-08 11:46:31 -0800

    added swatches

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
       fadeOut: false,
       mainText: false,
       userBrandInput: '',
-      colors: []
+      colors: [],
+      finalColours: []
     }
   }
 
@@ -25,20 +26,19 @@ class App extends Component {
     })
   }
 
-// what to do when user clicks desired colour
+  // what to do when user clicks desired colour
   handleSwatch = (e) => {
-      e.preventDefault();
-      //  go to the art page
+    e.preventDefault();
+    //  go to the art page
   }
 
-// store user brand input in state
+  // store user brand input in state
   handleChange = (e) => {
     this.setState({
       userBrandInput: e.target.value
     })
   };
 
-//   what to do when user submits search
   handleClick = (e) => {
     e.preventDefault();
     //   go to the swatches, batches
@@ -51,58 +51,163 @@ class App extends Component {
         brand: `${this.state.userBrandInput}`,
       }
     })
-    .then((res) => {
-      // console.log(res.data);
-      const colorsOnly = [];
-      for (let i in res.data) {
-        // console.log(res.data[i].product_colors);
-        const products = res.data[i].product_colors;
-        products.forEach((info) => {
-          // console.log(info);
-          colorsOnly.push(info.hex_value);
-          // console.log(colorsOnly);
+      .then((res) => {
+        // console.log(res.data);
+        const colorsOnly = [];
+        for (let i in res.data) {
+          // console.log(res.data[i].product_colors);
+          const products = res.data[i].product_colors;
+          products.forEach((info) => {
+            // console.log(info);
+            colorsOnly.push(info.hex_value);
+            // console.log(colorsOnly);
+          })
+        }
+        console.log(colorsOnly);
+        this.randomColours(colorsOnly)
+        this.setState({
+          colors: colorsOnly
         })
-      }
-      this.setState({
-        colors: colorsOnly
       })
-    })
   }
 
-  render() { 
+  randomColours = (array) => {
+    this.setState({
+      finalColours: []
+    })
+    const indexSize = 7
+    for (let i = 0; i <= (indexSize - 1); i++) {
+      const randomIndex = Math.floor(Math.random() * array.length)
+      if (array[randomIndex] !== array[i]) {
+        this.state.finalColours.push(array[randomIndex]);
+        console.log("SwatchArray", this.state.finalColours)
+      }
+    }
+    // sometimes repeats... how to fix?
+  }
+
+  render() {
     return (
       <div>
-      <header id="start">
-        <div className="banner">
-          <video src={video} autoPlay={this.state.play} loop="true"></video>
-          <div className="search">
-            <div onClick={this.playVideo} className={this.state.fadeOut ? 'hide' : null}>
-              <LoadingText />
+        <header id="start">
+          <div className="banner">
+            <video src={video} autoPlay={this.state.play} loop="true"></video>
+            <div className="search">
+              <div onClick={this.playVideo} className={this.state.fadeOut ? 'hide' : null}>
+                <LoadingText />
+              </div>
+              <h1 className={this.state.mainText ? 'showText' : null}>Hue are you?</h1>
+              <form action="" className={`search-box ${this.state.mainText ? 'showSearch' : null}`}>
+                <label htmlFor="search" className="sr-only">Search a make-up brand</label>
+                <input id="search" className="search-text" type="text" placeholder="Search a make-up brand" onChange={this.handleChange} />
+                <button className="search-button" onClick={this.handleClick} type="submit"><img src={paint} alt="paint splash icon" /></button>
+              </form>
             </div>
-            <h1 className={this.state.mainText ? 'showText' : null}>Hue are you?</h1>
-            <form action="" className={`search-box ${this.state.mainText ? 'showSearch' : null}`}>
-              <label htmlFor="search" className="sr-only">Search a make-up brand</label>
-              <input id="search" className="search-text" type="text" placeholder="Search a make-up brand" onChange={this.handleChange}/>
-              <button className="search-button" onClick={this.handleClick} type="submit"><img src={paint} alt="paint splash icon"/></button>
-            </form>
           </div>
-        </div>
-      </header>
-      {/* Render swatches:
-        <div className="swatches">
-          {
-            this.state.colors.map((color, index) => (
-              <li key=(index)>
-                <button className="swatch (color)" style={(color)} onClick={this.handleSwatchClick}></button>
-              </li>
-            )
-          }
-        </div> */}
-        {/* <Swatches />
-        <Artwork /> */}
-    </div>  
+        </header>
+
+        {/* Render swatches: */}
+        <section className="swatches">
+          <ul>
+            {
+              this.state.finalColours.map((color, index) => {
+                const styles = {
+                  backgroundColor: (color)
+                };
+                console.log(color);
+                return (
+                  <li className="swatch" key={index}>
+                    <h2 style={styles}>{color}</h2>
+                    <button style={styles}>
+                    </button>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </section>
+      </div>
     )
   }
 }
 
 export default App
+
+
+
+  //   what to do when user submits search
+  // handleClick = (e) => {
+  //   e.preventDefault();
+  //   //   go to the swatches, batches
+  //   axios({
+  //     url: `https://makeup-api.herokuapp.com/api/v1/products.json`,
+  //     method: `GET`,
+  //     responseType: `json`,
+  //     params: {
+  //       // brand: {this.userBrandInput} (maybe state or props)
+  //       brand: `${this.state.userBrandInput}`,
+  //     }
+  //   })
+  //     .then((res) => {
+  //       // console.log(res.data);
+  //       const colorsOnly = [];
+  //       for (let i in res.data) {
+  //         // console.log(res.data[i].product_colors);
+  //         const products = res.data[i].product_colors;
+  //         products.forEach((info) => {
+  //           // console.log(info);
+  //           colorsOnly.push(info.hex_value);
+  //           // console.log(colorsOnly);
+  //         })
+  //       }
+  //       // console.log(colorsOnly)
+  //       this.randomColours(colorsOnly)
+  //       if (colorsOnly.length > 7) {
+  //         const smallArray = colorsOnly.splice(0, 7);
+  //         this.setState({
+  //           finalColours: smallArray
+  //         })
+  //       } else {
+  //         const smallArray = colorsOnly;
+  //         this.setState({
+  //           colors: smallArray
+  //         })
+  //       }
+  //     })
+  // }
+
+  // shuffle = (array) => {
+  //   let currentIndex = array.length, temporaryValue, randomIndex;
+  //   // While there remain elements to shuffle...
+  //   while (0 !== currentIndex) {
+  //     // Pick a remaining element...
+  //     randomIndex = Math.floor(Math.random() * currentIndex);
+  //     currentIndex -= 1;
+  //     // And swap it with the current element.
+  //     temporaryValue = array[currentIndex];
+  //     array[currentIndex] = array[randomIndex];
+  //     array[randomIndex] = temporaryValue;
+  //   }
+  //   return array;
+  // }
+
+  //   what to do when user submits search
+
+
+
+  // randomColours = (array) => {
+  //   this.setState({
+  //     finalColours: []
+  //   })
+  //   const indexSize = 7
+  //   for (let i = 0; i <= (indexSize - 1); i++) {
+  //     const randomIndex = Math.floor(Math.random() * array.length)
+  //     if (array[randomIndex] !== array[i]) {
+  //       this.setState({
+  //         finalColours: array
+  //       })
+  //       // this.state.finalColours.push(array[randomIndex]);
+  //       console.log(this.state.finalColours)
+  //     }
+  //   }
+  // }
