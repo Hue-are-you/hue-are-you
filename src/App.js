8e520917490e52b73cf323f4631a0576024c528b
christineshiels <christineshiels@gmail.com>
2020-12-08 10:58:53 -0800

    Christine's overwritten work 2020-12-07 random colour array functions

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { Component } from 'react';
 import video from './assets/smoke.mp4';
 import paint from './assets/paint-splash.png';
 import axios from 'axios';
-import Intro from './Intro.js';
-import TitleSvg from './TitleSvg';
+import LoadingText from './LoadingText.js';
 
 class App extends Component {
 
@@ -13,9 +12,9 @@ class App extends Component {
       play: false,
       fadeOut: false,
       mainText: false,
-      userBrandInputChange: "",
-      userBrandInputFinal: "",
+      userBrandInput: '',
       colors: [],
+      finalColours: []
     }
   }
 
@@ -27,66 +26,73 @@ class App extends Component {
     })
   }
 
-  componentDidMount() {
+  randomColours = (array) => {
+    this.setState ({
+      finalColours:[],
+      colors: []
+    })
+    const indexSize = 7
+    for (let i=0; i <= (indexSize - 1); i++) {
+      const randomIndex = Math.floor(Math.random() * array.length)
+      if (array[randomIndex] !== array[i]) {
+       this.state.colours.push(array[randomIndex]);
+        console.log("SwatchArray", this.state.colors)
+        // Calvin suggested creation of an additional array to set state
+        this.setState({
+          finalColours: colors
+        })
+        
+      }
+    }
+  }
+
+// what to do when user clicks desired colour
+  handleSwatch = (e) => {
+      e.preventDefault();
+      //  go to the art page
+  }
+
+// store user brand input in state
+  handleChange = (e) => {
+    this.setState({
+      userBrandInput: e.target.value
+    })
+  };
+
+//   what to do when user submits search
+  handleClick = (e) => {
+    e.preventDefault();
+    
+    //   go to the swatches, batches
     axios({
       url: `https://makeup-api.herokuapp.com/api/v1/products.json`,
       method: `GET`,
       responseType: `json`,
       params: {
-        brand: "fenty"
-        // (maybe state or props)
+        // brand: {this.userBrandInput} (maybe state or props)
+        brand: `${this.state.userBrandInput}`,
       }
     })
-    .then( (res)=> {
-      console.log("returned Makeup Array", res.data);
-      // ^this takes forever because it is calling ALL brands... when we have a user-selected brand, it will be quicker!
-      if (res.data.length > 6) {
-        const shortenedArray = res.data.slice(0, 7)
-        const coloursOnly = []
-        shortenedArray.map( (product)=> {
-          coloursOnly.push(product.product_colors[0].hex_value)
+    .then((res) => {
+      // console.log(res.data);
+      const colorsOnly = [];
+      for (let i in res.data) {
+        // console.log(res.data[i].product_colors);
+        const products = res.data[i].product_colors;
+        products.forEach((info) => {
+          // console.log(info);
+          colorsOnly.push(info.hex_value);
+          // console.log(colorsOnly);
         })
-        this.setState({
-          color: coloursOnly,
-        })
-      } else {
-        // basically same as above
-        console.log(res);
       }
-      // console.log(this.state.palette)
-    })
-  }
-
-// what to do when user clicks desired colour
- handleSwatch = (e) => {
-     e.preventDefault();
-    //  go to the art page
- }
-
-// store user brand input in state
-  handleChange = (e) => {
-    const target = e.target;
-    const value = target.value;
-    console.log(value);
-    this.setState({
-      userBrandInputChange: value
-    })
-    console.log("change", this.state.userBrandInputChange);
-  };
-
-//   what to do when user submits search
-  handleClick = (e) => {
-      e.preventDefault();
-      
       this.setState({
-        userBrandInputFinal: this.state.userBrandInputChange
+        colors: colorsOnly
       })
-      console.log("final", this.state.userBrandInputFinal);
-    //   go to the swatches, batches
-      
+      this.randomColours(colorsOnly)
+    })
   }
 
-  render() {
+  render() { 
     return (
       <div>
       <header id="start">
@@ -94,29 +100,26 @@ class App extends Component {
           <video src={video} autoPlay={this.state.play} loop="true"></video>
           <div className="search">
             <div onClick={this.playVideo} className={this.state.fadeOut ? 'hide' : null}>
-              <Intro/>
+              <LoadingText />
             </div>
-            <h1 className="sr-only">Hue are you?</h1>
-            <div className={this.state.mainText ? 'showText' : null}>
-              <TitleSvg/>
-            </div>
+            <h1 className={this.state.mainText ? 'showText' : null}>Hue are you?</h1>
             <form action="" className={`search-box ${this.state.mainText ? 'showSearch' : null}`}>
               <label htmlFor="search" className="sr-only">Search a make-up brand</label>
-              <input id="search" className="search-text" type="text" placeholder="Search a make-up brand" onChange={ (e) => {this.handleChange(e)}}/>
-              <button className="search-button" onClick={ (e) => {this.handleClick(e)}} type="submit"><img src={paint} alt="paint splash icon"/></button>
+              <input id="search" className="search-text" type="text" placeholder="Search a make-up brand" onChange={this.handleChange}/>
+              <button className="search-button" onClick={this.handleClick} type="submit"><img src={paint} alt="paint splash icon"/></button>
             </form>
           </div>
         </div>
       </header>
       {/* Render swatches:
         <div className="swatches">
-            {
-                this.state.colors.map((color, index) => (
-                    <li key=(index)>
-                        <button className="swatch (color)" style={(color)} onClick={this.handleSwatchClick}></button>
-                    </li>
-                )
-            }
+          {
+            this.state.colors.map((color, index) => (
+              <li key=(index)>
+                <button className="swatch (color)" style={(color)} onClick={this.handleSwatchClick}></button>
+              </li>
+            )
+          }
         </div> */}
         {/* <Swatches />
         <Artwork /> */}
